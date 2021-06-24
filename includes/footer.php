04f4485e69ecf8de92f0ec645c0ba3480dad074d
MohamedZhran <mohamedzhran1@gmail.com>
2021-06-24 16:16:50 +0200

    fix footer

--- includes/footer.php
+++ includes/footer.php
@@ -5,7 +5,7 @@
         <div class="row">
           <div class="col-sm-5">
             <div class="width-200">
-              <img alt='image' src="<? echo $images; ?>/logo/Logo (MEA) W.png">
+              <img alt='image' src="<? echo $imagesDir; ?>/logo/Logo (MEA) W.png">
             </div>
             <p>Malaysian Export Academy was registered in October 2007, to take over the activities of The Exporters
               Club of Malaysia specifically the Training & Education Unit.</p>
@@ -75,4 +75,5 @@
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 </body>
+
 </html>
