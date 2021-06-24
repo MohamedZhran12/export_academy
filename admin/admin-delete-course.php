1d07a5e739b9126d18325e75087adb8070677257
MohamedZhran <mohamedzhran1@gmail.com>
2021-06-24 20:39:41 +0200

    fix delete course

--- admin/admin-delete-course.php
+++ admin/admin-delete-course.php
@@ -3,7 +3,7 @@ require_once($_SERVER['DOCUMENT_ROOT'] . 'includes/init_admin.php');
 require_once($includes . 'sections_info.php');
 
 $sql = $conn->prepare("DELETE FROM $table WHERE sys_course_id = ?");
-$sql->execute([$id]);
+$sql->execute($_GET['id']);
 
 echo "
 	<script type='text/javascript'>alert('Delete Course Successfully!');
