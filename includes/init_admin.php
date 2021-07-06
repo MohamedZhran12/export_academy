23ae89a4f9cadd5885746090a7cff275af517ffe
MohamedZhran <mohamedzhran1@gmail.com>
2021-07-06 20:20:56 +0200

    refactor init files

--- includes/init_admin.php
+++ includes/init_admin.php
@@ -1,19 +1,5 @@
 <?php
-session_start();
-ini_set('display_errors', 0);
-ini_set('display_startup_errors', 0);
-// error_reporting(E_ALL);
-
-$css = '/assets/css/';
-$js = '/assets/js/';
-$imagesDir = '/images/';
-$rootDir = $_SERVER['DOCUMENT_ROOT'] . '/';
-$includes = $rootDir . '/includes/';
-
-require_once($includes . "config.php");
-require_once($includes . "sys_config.php");
-require_once($includes . "func_date.php");
-require_once($includes . "func_utility.php");
+require_once('init_base.php');
 require_once($includes . "admin_header.php");
 require_once($includes . "admin_nav.php");
 
