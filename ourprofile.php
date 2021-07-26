ffe4d411d2d65d6567628094252eaed35a8a8491
MohamedZhran <mohamedzhran1@gmail.com>
2021-07-26 23:53:24 +0200

    edit our profile document

--- ourprofile.php
+++ ourprofile.php
@@ -30,94 +30,17 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/includes/init.php");
           <h2 style="margin-top: 0in;margin-bottom: 0in;text-align: justify;background: transparent;font-family: Arial, serif;font-size:21px;font-weight: bold;line-height: 150%;">INTRODUCTION</h2>
         </li>
       </ol>
-      <p style="text-align: justify;background: transparent;line-height: 150%;">Malaysian Export Academy was registered in October 2007 as a Company with the registrar of Companies in Malaysia. Its main activities are training and education. The Academy has been certified ISO 9001:2015.</p>
-      <br><br>
-      <p style="text-align: justify;background: transparent;line-height: 150%;">The Academy has been registered with the Human Resources Development Corporation, Ministry of Human Resources of Malaysia. In this respect, almost all of its training programmes are fully subsidized by the said Fund. The Academy is also registered with the Ministry of Finance of Malaysia.&nbsp;The Academy has been awarded the 5 star status&nbsp;by the Human Resources Development Fund in 2016</p>
-      <br><br>
-      <p style="text-align: justify;background: transparent;line-height: 150%;">The Academy has won many government tenders to conduct short courses and professional programmes under various schemes that included 1 Malaysia Globally Recognised Industry and Professionals Programme, Minimum Wages Training Incentive, SME Senior Managers Development Programme and SME Upskilling Programmes.</p>
+      <p>Malaysian Export Academy was registered in October 2007 as a Company with the registrar of Companies in Malaysia. Its main activities are training and education. The Academy has been certified ISO 9001:2015.
+        <br><br>
+        The Academy has been registered with the Human Resources Development Corporation, Ministry of Human Resources of Malaysia. In this respect, almost all of its training programmes are fully subsidized by the said Fund. The Academy is also registered with the Ministry of Finance of Malaysia. The Academy has been awarded the 5 star status by the Human Resources Development Fund in 2016
+        <br><br>
+        The Academy has won many government tenders to conduct short courses and professional programmes under various schemes that included 1 Malaysia Globally Recognised Industry and Professionals Programme, Minimum Wages Training Incentive, SME Senior Managers Development Programme and SME Upskilling Programmes.
+        <br><br>
+        The Academy trains and coaches an average of 3,500 participants per year on various programmes in Malaysia and also in other countries in the ASEAN region.
+      </p>
       <br><br>
       <p style="text-align: justify;background: transparent;line-height: 150%;">The Academy trains and coaches an average of 3,500 participants per year on various programmes in Malaysia and also in other countries in the ASEAN region.</p>
       <br><br>
-      <p style="text-align: justify;background: transparent;line-height: 150%;">The Academy is a member of the following Local and International organisations:</p>
-      <ul>
-        <li>
-          <p style="text-align: justify;background: transparent;margin-bottom: 0in;line-height: 150%;">International Association of Trade Training Organisations (IATTO)</p>
-        </li>
-        <li>
-          <p style="text-align: justify;background: transparent;margin-bottom: 0in;line-height: 150%;">Malaysian Exporters Association</p>
-        </li>
-        <li>
-          <p style="text-align: justify;background: transparent;margin-bottom: 0in;line-height: 150%;">Malaysian Training Providers Association</p>
-        </li>
-        <li>
-          <p style="text-align: justify;background: transparent;margin-bottom: 0in;line-height: 150%;">Malaysian External Trade Development Corporation</p>
-        </li>
-        <li>
-          <p style="text-align: justify;background: transparent;margin-bottom: 0in;line-height: 150%;">Malaysian International Chamber of Commerce and Industry</p>
-        </li>
-      </ul>
-      <br><br>
-      <p style="text-align: justify;background: transparent;line-height: 150%;">The Academy runs various certification and professional programmes in collaboration with the following International and Local partners:</p>
-      <br><br>
-      <ul>
-        <li>
-          <p style="text-align: justify;background: transparent;margin-bottom: 0in;line-height: 150%;">International Trade Council, USA</p>
-        </li>
-        <li>
-          <p style="text-align: justify;background: transparent;margin-bottom: 0in;line-height: 150%;">CCSD Council, Prague</p>
-        </li>
-        <li>
-          <p style="text-align: justify;background: transparent;margin-bottom: 0in;line-height: 150%;">International Association of Accounting Professional (IAAP), UK</p>
-        </li>
-        <li>
-          <p style="text-align: justify;background: transparent;margin-bottom: 0in;line-height: 150%;">Chartered Management Institute, UK</p>
-        </li>
-        <li>
-          <p style="text-align: justify;background: transparent;margin-bottom: 0in;line-height: 150%;">UBIS University of Switzerland</p>
-        </li>
-        <li>
-          <p style="text-align: justify;background: transparent;margin-bottom: 0in;line-height: 150%;">European International University, France</p>
-        </li>
-        <li>
-          <p style="text-align: justify;background: transparent;margin-bottom: 0in;line-height: 150%;">Alibaba Business School, China</p>
-        </li>
-        <li>
-          <p style="text-align: justify;background: transparent;margin-bottom: 0in;line-height: 150%;">Genovasi University College, Malaysia</p>
-        </li>
-      </ul>
-      <br><br>
-      <p style="text-align: justify;background: transparent;line-height: 150%;">Besides training and education, the Academy is also involved in seminars/conferences, consultancy, research &amp; development and publications.</p>
-      <br><br>
-      <ol start="2" type="A">
-        <li>
-          <h1 style='margin-top: 0in;margin-bottom: 0in;text-align: justify;background: transparent;font-family: "Times New Roman", serif;font-size:19px;font-weight: bold;line-height: 150%;'>VISION &amp; MISSION</h1>
-        </li>
-      </ol>
-      <p style="text-align: justify;background: transparent;line-height: 108%;"><br>&nbsp;</p>
-      <p style="text-align: justify;background: transparent;line-height: 150%;">Our Vision is to be a leading Training Provider in the ASEAN region.</p>
-      <br><br>
-      <p style="text-align: justify;background: transparent;line-height: 150%;">Our Mission is to train and coach entrepreneurs and would be entrepreneurs to acquire Practical knowledge and skills in various fields including exporting and international trade</p>
-      <br><br>
-      <ol start="3" type="A">
-        <li>
-          <p style="text-align: justify;background: transparent;margin-bottom: 0in;line-height: 150%;"><strong>OBJECTIVES</strong></p>
-        </li>
-      </ol>
-      <p style="text-align: justify;background: transparent;line-height: 150%;">Our main objectives are:</p>
-      <ul>
-        <li>
-          <p style="margin-bottom: 0in;line-height: 150%;text-align: left;background: transparent;">To provide professional, quality and industry driven practical training and education in the field of Export and International Trade, Logistics and Supply Chain Management, Human Resource Development, Tourism &amp; Hospitality, Event Management, Taxation &amp; Finance, Leadership and other skill programmes.</p>
-        </li>
-      </ul>
-      <p style="margin-bottom: 0in;line-height: 150%;text-align: left;background: transparent;"><br></p>
-      <ul>
-        <li>
-          <p style="margin-bottom: 0in;line-height: 150%;text-align: left;background: transparent;">To collaborate with other Training and Education Institutions to offer variety of quality programmes in Malaysia and other ASEAN countries<br>&nbsp;</p>
-        </li>
-      </ul>
-      <p style="text-align: center;background: transparent;line-height: 108%;">To promote the culture of e-learning and blended learning in the region</p>
-      <p style="line-height: 108%;text-align: left;background: transparent;"><br>&nbsp;</p>
-
     </div>
   </div>
 
@@ -128,28 +51,29 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/includes/init.php");
         <div class="padding-100">
           <div class="row">
             <div class="col-sm-6">
-              <b>In the field of International Trade and Export Training, the Academy’s collaborative partners are as
-                follows:</b><br><br>
+              <b>The Academy is a member of the following Local and International organisations:</b><br><br>
               <ul>
-                <li>Malaysian Institute of Export & International Trade
-                <li>Institute of Export, United Kigdom
-                <li>University Utara Malaysia
-                <li>University of Kuala Lumpur
-                <li>Asia e-University
-                <li>Institute of Leadership and Management
-                <li>Australian College of Business & Management.
+                <li>International Association of Trade Training Organisations (IATTO)
+                <li>Malaysian Exporters Association
+                <li>Malaysian Training Providers Association
+                <li>Malaysian External Trade Development Corporation
+                  Malaysian International Chamber of
+                  Commerce and Industry
               </ul>
               </p>
             </div>
 
             <div class="col-sm-6">
-              <b>The Academy is also a member of the following organisations:</b><br><br><br>
+              <b>The Academy runs various certification and professional programmes in collaboration with the following International and Local partners:</b><br><br><br>
               <ul>
-                <li>International Association of Trade Training Organisations (IATTO)</li>
-                <li>Malaysian Exporters Association</li>
-                <li>Malaysian Training Providers Association</li>
-                <li>National Association of Private Educational Institutions</li>
-                <li>Malaysian External Trade Development Corporation</li>
+                <li>International Trade Council, USA</li>
+                <li>CCSD Council, Prague</li>
+                <li>International Association of Accounting Professional (IAAP), UK </li>
+                <li>Chartered Management Institute, UK </li>
+                <li>UBIS University of Switzerland </li>
+                <li>European International University, France</li>
+                <li>Alibaba Business School, China</li>
+                <li>Genovasi University College, Malaysia</li>
               </ul>
 
             </div>
@@ -182,25 +106,18 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/includes/init.php");
         <div class="col-sm-6">
           <div class="tab-content" id="myTabContent">
             <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-              <p><u>Our Vision</u> is to be a premier training provider in the ASEAN region focusing on professional skills
-                and workforce development.
+              <p><u>Our Vision</u> is to be a leading Training Provider in the ASEAN region.
                 <br><br>
-                Our Mission is to provide high quality and world class training programmes to support workforce
-                development, thereby enhancing their contributions to the goals of their organization.
+                <u>Our Mission</u> is to train and coach entrepreneurs and would be entrepreneurs to acquire Practical knowledge and skills in various fields including exporting and international trade
               </p>
             </div>
 
             <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
               <p>
               <ul>
-                <li class='mb-4'>To provide professional, quality and industry driven practical training and education in the field
-                  of export and international trade
+                <li class='mb-4'>To provide professional, quality and industry driven practical training and education in the field of Export and International Trade, Logistics and Supply Chain Management, Human Resource Development, Tourism & Hospitality, Event Management, Taxation & Finance, Leadership and other skill programmes.
                 </li>
-                <li class='mb-4'>To develop and market the Academy’s programmes in the ASEAN region</li>
-                <li class='mb-4'>To collaborate with other International Trade Training Institutions to offer variety of quality
-                  programmes in Malaysia and other ASEAN countries
-                </li>
-                <li>To promote the culture of e-learning and blended learning in the region</li>
+                <li class='mb-4'>To collaborate with other Training and Education Institutions to offer variety of quality programmes in Malaysia and other ASEAN countries to promote the culture of e-learning and blended learning in the region</li>
               </ul>
               </P>
             </div>
