a9870fb25a8451d8d079c47591198322dcbeb6a0
MohamedZhran <mohamedzhran1@gmail.com>
2021-08-04 09:55:38 +0200

    restore fb chat

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,3 @@
-
 <html lang="en">
 
 <head>
@@ -32,3 +31,26 @@
     width: 100%;
     background: #fff;
     height: 100%'></div>
+  <!-- Load Facebook SDK for JavaScript -->
+  <div id="fb-root"></div>
+  <script>
+    window.fbAsyncInit = function() {
+      FB.init({
+        xfbml: true,
+        version: 'v8.0'
+      });
+    };
+
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  </script>
+
+  <!-- Your Chat Plugin code -->
+  <div class="fb-customerchat" attribution=setup_tool page_id="339868929556041">
+  </div>
